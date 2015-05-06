aee96258982ea199d8d2ba5eab6b15074f1b56e2
Djordje Dimitrijev <rasplinjac@gmail.com>
2015-05-06 15:04:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 var urlAdjuster = require('gulp-css-url-adjuster');
+var rename = require('gulp-rename');
 var files = [];
 
 /*
@@ -12,12 +13,13 @@ var files = [];
  |
  */
 
-elixir.extend('urlAdjuster', function(input, options, output) {
+elixir.extend('urlAdjuster', function(input, options, output, newname) {
 
     files.push({
         input: input,
         options: options,
-        output: output
+        output: output,
+        newname: newname
     });
 
     var stream;
@@ -25,8 +27,11 @@ elixir.extend('urlAdjuster', function(input, options, output) {
     gulp.task('urlAdjuster', function() {
         files.forEach(function(toUrlAdjust) {
             stream = gulp.src(toUrlAdjust.input)
-                         .pipe(urlAdjuster(toUrlAdjust.options))
-                         .pipe(gulp.dest(toUrlAdjust.output));
+            stream.pipe(urlAdjuster(toUrlAdjust.options));
+            if (typeof toUrlAdjust.newname !== 'undefined') {
+                stream.pipe(rename(toUrlAdjust.newname));
+            }
+            stream.pipe(gulp.dest(toUrlAdjust.output));
         });
         
         return stream;
